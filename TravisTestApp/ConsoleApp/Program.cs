f2d708c33b21c5d1e089d92230bad0647f85f720
Faruk Aydogan <aydoganf@itu.edu.tr>
2020-11-23 09:41:09 +0300

    projects

---
+++ TravisTestApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SomeBusinessLib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyService myService = new MyService();
+
+            var result = myService.Sum(1, 5);
+            Console.WriteLine($"Result is {result}");
+        }
+    }
+}
---
+++ TravisTestApp/SomeBusinessLib/MyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SomeBusinessLib
+{
+    public class MyService
+    {
+
+        public int Sum(int a, int b) => a + b;
+
+    }
+}
